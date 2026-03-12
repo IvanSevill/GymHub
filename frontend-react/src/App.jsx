c02622a72fecb82bb8bbc791f18247be921286d5
Ivan Sevillano <ivansevillano2005@gmail.com>
2026-03-12 11:14:50 +0100

    Provisional Frontend

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>GymHub</h1>
+        <p>Your ultimate workout companion</p>
+      </header>
+      <main>
+        {/* Components will go here */}
+        <p>Loading application...</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
